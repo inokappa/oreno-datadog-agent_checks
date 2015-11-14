c86cf41a5c11a611dceb408ee87754d3de231f83
inokappa <inokara@gamil.com>
2015-11-14 23:59:39 +0900

    check_user

---
+++ check_user/check_user.py
@@ -0,0 +1,25 @@
+import time
+import subprocess
+from checks import AgentCheck
+
+class CheckUser(AgentCheck):
+    def check(self, instance):
+        threshold_user = self.init_config.get('threshold_user', 3)
+        current_login_count = self.login_check()
+        self.gauge('login.user.check', current_login_count)
+        if int(current_login_count) >= int(threshold_user):
+        	self.send_event(current_login_count, 'error')
+
+    def login_check(self):
+        count = subprocess.check_output('w -sh | wc -l', shell=True)  
+        return count.strip()
+
+    def send_event(self, current_login_count, alert_type):
+        self.event({
+            'timestamp': int(time.time()),
+            'event_type': 'login_user_check',
+            'msg_title': 'Still Login user count check',
+            'msg_text': '%s users still login.' % (current_login_count),
+            'aggregation_key': 'login.user.check',
+            'alert_type': alert_type
+        })
